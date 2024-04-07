95e522afaf04fa6154306443b6d50db7f46841bf
Ely Deckers <e.deckers@gmail.com>
2024-04-08 00:07:11 +0200

    feat: make tag label allowlist configurable

--- src/packages/prometheus-api/prometheus_api/consts.py
+++ src/packages/prometheus-api/prometheus_api/consts.py
@@ -1,4 +1,7 @@
-from aws_meta_aggregator.consts import AWS_AGGREGATOR_RESOURCE_DEFAULT_ALLOWLIST
+from aws_meta_aggregator.consts import (
+    AWS_AGGREGATOR_RESOURCE_DEFAULT_ALLOWLIST,
+    AWS_AGGREGATOR_RESOURCE_TAG_DEFAULT_ALLOWLIST,
+)
 from prometheus_api.utils.environment import env
 
 APPLICATION_LOG_LEVEL = env("LOG_LEVEL", "DEBUG")
@@ -15,5 +18,12 @@ APPLICATION_PROMETHEUS_RESOURCE_LABEL_ALLOWLIST: list[str] = list(
     env(
         "RESOURCE_LABEL_ALLOWLIST",
         AWS_AGGREGATOR_RESOURCE_DEFAULT_ALLOWLIST,
+    ).split(" ")
+)
+
+APPLICATION_PROMETHEUS_RESOURCE_TAG_LABEL_ALLOWLIST: list[str] = list(
+    env(
+        "RESOURCE_TAG_LABEL_ALLOWLIST",
+        AWS_AGGREGATOR_RESOURCE_TAG_DEFAULT_ALLOWLIST,
     ).split(" ")
 )
